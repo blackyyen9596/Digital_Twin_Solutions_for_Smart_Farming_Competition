c106b0c5fbea43d031faf11343c23ef66dfa53d1
Chen-Yen Chang <66304059+BlackyYen@users.noreply.github.com>
2021-01-20 22:07:25 +0800

    Update

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # Digital_Twin_Solutions_for_Smart_Farming_Competition
 
 #### 🚧施工中
-#### [1] [利用pandas随机切分csv文件](https://www.cnblogs.com/bymo/p/7248993.html)@[焦距](https://home.cnblogs.com/u/bymo/)
+
+## 目錄
+
+1. [致謝](#致謝)
+
+## 致謝
+#### [1] 博客园：[利用pandas随机切分csv文件](https://www.cnblogs.com/bymo/p/7248993.html) @[焦距](https://home.cnblogs.com/u/bymo/)
+#### [2] 知乎：[如何通过sklearn实现多标签分类？](https://www.zhihu.com/question/271470776) @[德川Captain](https://www.zhihu.com/people/de-chuan-captain)
+#### [3] CSDN: [多标签分类的评价指标](https://blog.csdn.net/hzhj2007/article/details/79153647) @[hzhj](https://blog.csdn.net/hzhj2007)
 
--- dataset_partition.py
+++ dataset_partition.py
@@ -1,14 +1,42 @@
 import pandas as pd
+from pandas import DataFrame
+from tqdm import tqdm
+import random
 
 # 讀取資料集
-df = pd.read_csv(r'D:\dataset\2021智慧農業數位分身創新應用競賽\train_data.csv',
-                 encoding='utf-8')
-# 刪除重複的資料，並保留出現第一次的
-df.drop_duplicates(keep='first', inplace=True)
-# 將資料打亂
-df = df.sample(frac=1.0).reset_index(drop=True)
-cut_idx = int(round(0.9 * df.shape[0]))
-df_train, df_val = df.iloc[:cut_idx], df.iloc[cut_idx:]
+df = pd.read_csv(r'D:\dataset\2021智慧農業數位分身創新應用競賽\org\train_data.csv',
+                 index_col=0,
+                 encoding='utf-8')  #原始資料路徑
+val_idx = []  #用於紀錄該日期所有資料在該資料集中的index
+date_list = []  #用於儲存所有日期
+train_data = []  #用於儲存訓練資料
+val_data = []  #用於儲存驗證資料
+
+i = 0
+for time in df['d.log_time']:
+    date = time.split()[0]
+    date_list.append(date)
+date_set = set(date_list)
+val_date = random.choices(tuple(date_set), k=10)
+
+with tqdm(total=len(df)) as pbar:
+    for index, row in df.iterrows():
+        data_row = df.loc[index].values
+        time = data_row[0]
+        date = time.split()[0]
+
+        if date in val_date:
+            val_data.append(df.loc[index])
+        else:
+            train_data.append(df.loc[index])
+
+        # 更新進度條
+        pbar.update(1)
+        pbar.set_description('generate_dataset')
+
+df_val = DataFrame(val_data)
+df_train = DataFrame(train_data)
+
 # 輸出train.csv與val.csv檔案
-df_train.to_csv(r'D:\dataset\2021智慧農業數位分身創新應用競賽\train.csv', index=False)
-df_val.to_csv(r'D:\dataset\2021智慧農業數位分身創新應用競賽\val.csv', index=False)
+df_train.to_csv(r'D:\dataset\2021智慧農業數位分身創新應用競賽\dataset\train.csv')  #生成訓練資料路徑
+df_val.to_csv(r'D:\dataset\2021智慧農業數位分身創新應用競賽\dataset\val.csv')  #生成驗證資料路徑
