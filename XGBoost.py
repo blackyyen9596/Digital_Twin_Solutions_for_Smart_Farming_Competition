23b12b4e82aa78fb437b3b5f5bda0a121c56232c
Chen-Yen Chang <66304059+BlackyYen@users.noreply.github.com>
2021-01-23 05:03:59 +0800

    Update XGBoost.py

--- XGBoost.py
+++ XGBoost.py
@@ -7,11 +7,22 @@ import pandas as pd
 from sklearn.metrics import f1_score, hamming_loss
 from sklearn.decomposition import PCA
 args = {
-    'verbosity': 0,
+    'learning_rate': 0.01,
+    'n_estimators': 1000,
+    'booster': 'gbtree',
+    'objective': 'reg:tweedie',
+    'gamma': 0.1,
+    'max_depth': 6,
+    'min_child_weight': 5,
+    'reg_lambda': 0.2,
+    'reg_alpha': 0,
+    'seed': 2021,
+    'nthread': 12,  # cpu 執行緒數 
     'tree_method': 'gpu_hist',
     'eval_metric': 'logloss',
     'use_label_encoder': False,
 }
+
 clf_multilabel = OneVsRestClassifier(XGBClassifier(**args))
 
 csv_file = r'D:\dataset\2021智慧農業數位分身創新應用競賽\stacking\xgboost'
@@ -37,10 +48,10 @@ val_label = pd.read_csv(
     index_col=0,
 )
 
-pca = PCA(n_components=0.99)
-train_data = pca.fit_transform(train_data)
-val_data = pca.transform(val_data)
-test_data = pca.transform(test_data)
+# pca = PCA(n_components=0.99)
+# train_data = pca.fit_transform(train_data)
+# val_data = pca.transform(val_data)
+# test_data = pca.transform(test_data)
 
 clf_multilabel.fit(train_data, train_label)
 val_pred = clf_multilabel.predict(val_data)
@@ -51,7 +62,7 @@ test_pres.columns = [
     'actuator06', 'actuator07', 'actuator08', 'actuator09', 'actuator10',
     'actuator11'
 ]
-test_pres.to_csv(r'D:\dataset\2021智慧農業數位分身創新應用競賽\results\result_012221.csv')
+test_pres.to_csv(r'D:\dataset\2021智慧農業數位分身創新應用競賽\results\result_012321.csv')
 
 # 輸出模型評估指標
 print('f1_score:', round(f1_score(val_label, val_pred, average="macro"), 4))
