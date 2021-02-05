1aa602bd83a5c3cf76064f2fe2a4b84272519366
Chen-Yen Chang <66304059+BlackyYen@users.noreply.github.com>
2021-02-05 20:21:05 +0800

    Update

--- utils/utils.py
+++ utils/utils.py
@@ -1,8 +1,36 @@
 from torch.utils.data import DataLoader
 from torchvision import datasets
+from torch.utils.data import Dataset
+import pandas as pd
+import numpy as np
 from prefetch_generator import BackgroundGenerator
 
 
 class DataLoaderX(DataLoader):
     def __iter__(self):
         return BackgroundGenerator(super().__iter__())
+
+
+class Setloader(Dataset):
+    def __init__(self, data, label):
+        super(Setloader, self).__init__()
+        self.data = data
+        self.label = label
+
+    def __getitem__(self, index):
+        return self.data[index], self.label[index]
+
+    def __len__(self):
+        return self.data.shape[0]
+
+
+class TestSetloader(Dataset):
+    def __init__(self, data):
+        super(TestSetloader, self).__init__()
+        self.data = data
+
+    def __getitem__(self, index):
+        return self.data[index]
+
+    def __len__(self):
+        return self.data.shape[0]
